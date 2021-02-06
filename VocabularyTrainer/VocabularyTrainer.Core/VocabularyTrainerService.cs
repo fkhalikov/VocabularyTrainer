de7afab01179bb1fe9d2e4125616f9b5be13f51f
fkhalikov <fkhalikov@gmail.com>
2021-02-06 20:31:58 +0000

    New words

---
+++ VocabularyTrainer/VocabularyTrainer.Core/VocabularyTrainerService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VocabularyTrainer.Core
+{
+  public class Class1
+  {
+  }
+}
---
+++ VocabularyTrainer/VocabularyTrainer.Ports/Word.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VocabularyTrainer.Ports
+{
+  public class Word
+  {
+    public string English { get; set; }
+
+    public string German { get; set; }
+  }
+}
